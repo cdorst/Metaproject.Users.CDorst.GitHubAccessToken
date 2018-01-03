b3eecf20004aea6f55857f6a9ec3632dd9f68bad
Christopher Dorst <chris@dorst.email>
2018-01-03 04:40:53 -0600

    Add GitHubCredentials

---
+++ Metaproject.Users.CDorst.GitHubAccessToken/GitHubCredentials.cs
@@ -0,0 +1,12 @@
+﻿using Common.Structures.HttpBasicAuthentication;
+using static DevOps.VersionControl.Functions.GetGitHubApiCredentials.GitHubApiCredentials;
+
+namespace Metaproject.Users.CDorst.GitHubAccessToken
+{
+    public static class GitHubCredentials
+    {
+        private const string cdorst = nameof(cdorst);
+
+        public static BasicAuthenticationCredentials CDorst => GetCredentials(cdorst);
+    }
+}
